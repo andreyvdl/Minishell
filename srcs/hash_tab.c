e7cad6104eae597e0c692590aeba91f394ab5e20
afelipe- <andersonfelpe123@gmail.com>
2023-04-26 15:02:17 -0300

    (finish): HashTable functions

--- srcs/hash_tab.c
+++ srcs/hash_tab.c
@@ -1,6 +1,6 @@
 #include "../includes/minishell.h"
 
-unsigned int hash_code(char *key)
+static unsigned int hash_code(char *key)
 {
 	unsigned long	hash = 5381;
 	unsigned int	c;
@@ -20,3 +20,22 @@ t_node *create_node(char *key, char *value){
 	return (node);
 }
 
+void	insert_node(t_hash *hash, char *key, char *value)
+{
+	unsigned const int index = hash_code(key);
+	t_node *node = hash->list[index];
+	if (node == NULL)
+		hash->list[index] = create_node(key, value);
+		else
+		{
+			while (node)
+			{
+				if (node->next)
+				{
+					node->next = create_node(key, value);
+					break;
+				}
+				node = node->next;
+			}
+		}
+}
