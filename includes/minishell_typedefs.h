8463e9974871872eead13cc26b4d38ae5672caf2
adantas- <adantas-@student.42sp.org.br>
2023-04-26 14:08:39 -0300

    +:struct da hash e do node feitas

---
+++ includes/minishell_typedefs.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_typedefs.h                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 14:05:10 by adantas-          #+#    #+#             */
+/*   Updated: 2023/04/26 14:08:03 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_TYPEDEFS_H
+# define MINISHELL_TYPEDEFS_H
+
+# define HASH_SIZE 100
+
+typedef struct s_node {
+	char			*key;
+	char			*value;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_hash {
+	t_node	*list[HASH_SIZE];
+}	t_hash;
+
+#endif
