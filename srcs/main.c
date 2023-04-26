39226f6cd079a21e3500b751891ba564d3f03e01
adantas- <adantas-@student.42sp.org.br>
2023-04-26 14:21:59 -0300

    M:header da 42 removido pro git n encher o saco e tenho q testar a main

--- srcs/main.c
+++ srcs/main.c
@@ -1,8 +1,26 @@
 #include "../includes/minishell.h"
 
+int	main(int argc, char **argv, char **envp)
+{
+	char	*line;
 
-int main() {
-    while (1)
-        readline("mini> ");
-    return 0;
+	line = NULL;
+	while (true)
+	{
+		line = readline("mini> ");
+		if (line == NULL)
+		{
+			ft_putstr("exit\n");
+			break ;
+		}
+		else if (*line != '\0')
+		{
+			printf("Voce escreveu: [%s]\n", line);
+			free(line);
+		}
+	}
+	// A gente nao vai usar eles a nao ser que queira fazer easter egg
+	(void)argc;
+	(void)argv;
+	return (0);
 }
