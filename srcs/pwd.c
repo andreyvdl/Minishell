1f14d73086c4400fad80d22c39d09c0d221ee709
afelipe- <andersonfelpe123@gmail.com>
2023-05-01 05:28:08 -0300

    (add): pwd base 'finish'

--- srcs/pwd.c
+++ srcs/pwd.c
@@ -1,12 +1,27 @@
 #include "../includes/minishell.h"
 
-void	pwd(char *str, t_hash *hash)
+static int	syntax_error(char *str)
 {
-	while (*str && (*str != ' ' &&  *str != '\t'))
+
+	while (*str && (*str != ' ' && *str != '\t'))
 		str++;
 	while (*str && (*str == ' ' || *str == '\t'))
 		str++;
-	ft_putstr(str);
-	ft_putchar('\n');
-	(void)hash;
+	if (*str >= 33 && *str <= 126)
+		return (-1);
+	return (1);
+}
+
+void	pwd(char *str, t_hash *hash)
+{
+	if (syntax_error(str) == -1)
+	{
+		while (*str && (*str != ' ' && *str != '\t'))
+			str++;
+		while (*str && (*str == ' ' || *str == '\t'))
+			str++;
+		ft_printf("pwd: bad option: %s\n", str);
+	}
+	else
+		ft_printf("%s\n", search(hash, "PWD"));
 }
