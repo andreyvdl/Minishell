812edff435b6ffe288a891457c4a73704c638ed1
Akhashi <andersonfelpe123@gmail.com>
2023-05-03 06:22:17 -0300

    (add): addict struct for tokens for parser

--- includes/minishell.h
+++ includes/minishell.h
@@ -9,21 +9,30 @@
 #include "./minishell_typedefs.h"
 #include "../libs/libft/includes/libft.h"
 
+// int return
 int		cd(char *str);
+int		intersections(char *str, char inter);
+int		built_ins(char *input, t_hash *hash, char actions);
+
+//void return
 void	env(t_hash *hash);
-char	*separator(char *str);
 void	pwd(char *str, t_hash *hash);
 void	echo(char *str, t_hash *hash);
 void	unset(char *str, t_hash *hash);
 void	export(char *str, t_hash *hash);
 void	parser(char *str, t_hash *hash);
-char	*search(t_hash *hash, char *key);
 void	builtins(char *input, t_hash *hash);
-int		intersections(char *str, char inter);
-char	*expand_vars(char *str, t_hash *hash);
-t_node	*create_node(char *key, char *value);
 void	insert_node(t_hash *hash, char *key, char *value);
 void	inside_quote_copy(char **str, char **new, char quote);
 void	inside_quote_counter(char **str, size_t *counter, char quote);
 void	copy_with_expansions(char *str, char *new, t_hash *hash);
+
+// char retunr
+char	*separator(char *str);
+char	*search(t_hash *hash, char *key);
+char	*expand_vars(char *str, t_hash *hash);
+
+// t_node return
+t_node	*create_node(char *key, char *value);
+
 #endif // minishell.h
--- includes/minishell_typedefs.h
+++ includes/minishell_typedefs.h
@@ -13,4 +13,20 @@ typedef struct s_hash {
 	t_node	*list[HASH_SIZE];
 }	t_hash;
 
+typedef	enum
+{
+	WORD,
+	META_CHAR,
+	COMMAND,
+	PIPE
+}	e_token_type;
+
+typedef struct s_tokens
+{
+	char			*value;
+	e_token_type	type;
+	int				len;
+	struct s_tokens	*next;
+}	t_tokens;
+
 #endif
