a58aeb92da915e349f892301885864727cf58bd0
adantas- <adantas-@student.42sp.org.br>
2023-05-04 17:40:31 -0300

    M:changed the function to not receive address of pointer

--- libs/libft/sources/Memory/ft_free.c
+++ libs/libft/sources/Memory/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 11:19:26 by adantas-          #+#    #+#             */
-/*   Updated: 2023/04/25 11:25:24 by adantas-         ###   ########.fr       */
+/*   Updated: 2023/05/04 16:58:04 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
  * 
  * @param pointer A pointer to the address of the pointer to be freed;
  */
-void	ft_free(void **pointer)
+void	ft_free(void *pointer)
 {
-	if (*pointer != NULL)
+	if (pointer != NULL)
 	{
-		free(*pointer);
-		*pointer = NULL;
+		free(pointer);
+		pointer = NULL;
 	}
 }
