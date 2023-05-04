76d6a333f6fa39bf0f525682d2e388887c48d99f
adantas- <adantas-@student.42sp.org.br>
2023-05-04 17:38:56 -0300

    M:removed token struct

--- includes/minishell_typedefs.h
+++ includes/minishell_typedefs.h
@@ -13,20 +13,4 @@ typedef struct s_hash {
 	t_node	*list[HASH_SIZE];
 }	t_hash;
 
-typedef	enum
-{
-	WORD,
-	META_CHAR,
-	COMMAND,
-	PIPE
-}	e_token_type;
-
-typedef struct s_tokens
-{
-	char			*value;
-	e_token_type	type;
-	int				len;
-	struct s_tokens	*next;
-}	t_tokens;
-
 #endif
