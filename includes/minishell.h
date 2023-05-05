98d3b4487e6a65ccc2964ade63733d8150b40418
adantas- <adantas-@student.42sp.org.br>
2023-05-05 15:02:18 -0300

    M:just moved to the right position

--- includes/minishell.h
+++ includes/minishell.h
@@ -11,6 +11,7 @@
 
 // int return
 int		cd(char *str);
+int		parser(char *str, t_hash *hash);
 int		pipe_case(char **splited_pline);
 int		built_ins(char *input, t_hash *hash, char actions);
 int		write_to_case(char **splited_pline);
@@ -24,10 +25,12 @@ void	pwd(char *str, t_hash *hash);
 void	echo(char *str, t_hash *hash);
 void	unset(char *str, t_hash *hash);
 void	export(char *str, t_hash *hash);
-void	parser(char *str, t_hash *hash);
 void	builtins(char *input, t_hash *hash);
+void	free_hash(t_hash *hash);
 void	insert_node(t_hash *hash, char *key, char *value);
 void	easy_splitter(char *str);
+void	add_to_history(char *pipeline);
+void	free_all_and_exit(t_hash *hash);
 void	inside_quote_copy(char **str, char **new, char quote);
 void	inside_quote_counter(char **str, size_t *counter, char quote);
 void	copy_with_expansions(char *str, char *new, t_hash *hash);
@@ -41,8 +44,5 @@ char	*expand_vars(char *str, t_hash *hash);
 t_node	*create_node(char *key, char *value);
 
 // new functions, analyze they before putting in the right place
-void	free_hash(t_hash *hash);
-void	free_all_and_exit(t_hash *hash);
-void	add_to_history(char *pipeline);
 
 #endif // minishell.h
