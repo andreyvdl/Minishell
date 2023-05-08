e6102db7f373bed65695f3e02401baab569a2cce
adantas- <adantas-@student.42sp.org.br>
2023-05-08 17:45:44 -0300

    M:new struct and define protection

--- includes/minishell_typedefs.h
+++ includes/minishell_typedefs.h
@@ -1,7 +1,10 @@
 #ifndef MINISHELL_TYPEDEFS_H
 # define MINISHELL_TYPEDEFS_H
 
-# define HASH_SIZE 10
+# ifndef HASH_SIZE
+#  define HASH_SIZE 10
+# endif
+
 
 typedef struct s_node {
 	char			*key;
@@ -12,5 +15,17 @@ typedef struct s_node {
 typedef struct s_hash {
 	t_node	*list[HASH_SIZE];
 }	t_hash;
+
+typedef struct s_command {
+	char	*command;
+	char	*args[];
+	int		write_to;
+	int		read_from;
+}	t_command;
+
+typedef struct s_pipe {
+	t_command	*command[];
+	char		*envp[];
+}	t_pipe;
 
 #endif
