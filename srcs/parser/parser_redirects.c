ece5214d1bea0ed12891311f47b3b5afc183e908
adantas- <adantas-@student.42sp.org.br>
2023-05-09 17:46:13 -0300

    M:changed logic of the file

--- srcs/parser/parser_redirects.c
+++ srcs/parser/parser_redirects.c
@@ -1,92 +1,20 @@
 #include "../../includes/minishell.h"
 
-static int	extra_read_case(char *filename)
+int	redirect_invalid(char **split_pline, t_hash *hash)
 {
-	char	*temp;
-
-	temp = expand_vars(filename);
-	if (access(temp, R_OK) == -1)
+	while (*split_pline)
 	{
-		perror(RED"ms:read error: "RESET);
-		free(temp);
-		return (TRUE);
-	}
-	return (FALSE);
-}
-
-static int	invalid_read_case(char *filename)
-{
-	char	*temp;
-
-	if (ft_strchr(filename, '\'') != NULL)
-	{
-		remove_quote(filename);
-		if (access(filename, R_OK) == -1)
+		if (ft_strchr(*split_pline, '<') && !ft_strstr(*split_pline, "<<"))
 		{
-			perror(RED"ms:read error: "RESET);
-			return (TRUE);
-		}
-	}
-	else if (ft_strchr(filename, '\"') != NULL)
-	{
-		remove_quote(filename);
-		temp = expand_vars(filename);
-		if (access(temp, R_OK) == -1)
-		{
-			perror(RED"ms:read error: "RESET);
-			free(temp);
-			return (TRUE);
-		}
-	}
-	return (extra_read_case(filename));
-}
-
-static int	invalid_write_case(*filename)
-{
-	char	*temp;
-	int		fd;
-
-	if (ft_strchr(filename, '\'') != NULL)
-	{
-		remove_quote(filename);
-		fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR & S_IWUSR | S_IRGRP \
-		| S_IROTH);
-		if (fd == -1)
-		{
-			perror(RED"ms:write error: "RESET);
-			free(temp);
-			return (TRUE);
-		}
-	}
-	else if (ft_strchr(filename, '\"') != NULL)
-	{
-		remove_doubleq(filename);
-		temp = expand_vars(filename);
-		if (access(temp, W_OK) == -1)
-		{
-			perror(RED"ms:write error: "RESET);
-			free(temp);
-			return (TRUE);
-		}
-	}
-	return (FALSE);
-}
-
-int	redirect_invalid(char **splited_pline)
-{
-	while (*splited_pline)
-	{
-		if (ft_strchr("<", **splited_pline) && !ft_strstr(*splited_pline, "<<"))
-		{
-			if (invalid_read_case(*(splited_pline + 1)))
+			if (reading_invalid(*(split_pline + 1), hash))
 				return (TRUE);
 		}
-		else if (ft_strchr(">", **splited_pline))
+		else if (ft_strchr(*split_pline, '>'))
 		{
-			if (invalid_write_case(*(splited_pline + 1)))
+			if (writing_invalid(*(split_pline + 1), hash))
 				return (TRUE);
 		}
-		splited_pline++;
+		split_pline++;
 	}
 	return (FALSE);
 }
