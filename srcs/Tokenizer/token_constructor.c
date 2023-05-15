7d510a87291cc5c8274118a782dc1ade0ed8783d
adantas- <adantas-@student.42sp.org.br>
2023-05-15 17:46:18 -0300

    W:WIP on forking

--- srcs/Tokenizer/token_constructor.c
+++ srcs/Tokenizer/token_constructor.c
@@ -17,17 +17,44 @@ static char	**count_and_fill(char *input, size_t *n_cmds)
 	return (commands);
 }
 
+/* Arranja um lugar pra essa função depois */
+void	wait_child(pid_t pid, size_t n_cmds)
+{
+	int		wstatus;
+	size_t	count;
+
+	count = 1;
+	waitpid(pid, &wstatus, 0);
+	while (count < n_cmds)
+	{
+		wait(NULL);
+		count++;
+	}
+}
+
 void	tokenizer(char *input, t_hash *hash)
 {
 	char	**commands;
 	size_t	n_cmds;
+	pid_t	pid;
 
 	n_cmds = 0;
 	commands = count_and_fill(input, &n_cmds);
-	for (int i = 0; commands && commands[i]; i++){
-		redirection(commands[i]);
-	}
-	(void)hash;
+	if (n_cmds > 1)
+	// {
+	// 	while (*commands)
+	// 	{
+	// 		pid = set_up_multiple_cmd(commands, hash);
+	// 		commands++;
+	// 	}
+	// 	wait_child(pid, n_cmds);
+	// }
+	else if (n_cmds == 1 && isbuiltin(*commands))
+		// TODO: PAI.
+	else
+		// working_child(*commands, hash);
+	free(commands);
+}
 	/*
 	* os comandos estao em commands cada um em 1 string, todos com aspas, sem expandir e com os redirects;
 	todo: leitura/escrita de arquivos do filho.
@@ -42,4 +69,3 @@ void	tokenizer(char *input, t_hash *hash)
 		se os comandos forem builtins ele deve retornar o status pro pai
 		se os comandos nao forem builtins ele deve executar enquanto o pai espera o retorno
 	 */
-}
