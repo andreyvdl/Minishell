17580e3d2c20110689f783fc0bbfe9e40d236f77
Akhashi <andersonfelpe123@gmail.com>
2023-05-16 07:30:09 -0300

    (create): make builtin checker

--- srcs/essentials/isbuiltin.c
+++ srcs/essentials/isbuiltin.c
@@ -1,5 +1,27 @@
 #include "../../includes/minishell.h"
 
+
+int	isbuiltin(char *check)
+{
+	int		get;
+	int		i;
+	char	**builtin;
+	
+	builtin = ft_split(BUILTIN, ' ');
+	i = 0;
+	get = 0;
+	while (builtin && builtin[i])
+	{
+		if (ft_strcmp(builtin[i], check) == 0)
+		{
+			get = 1;
+			break;
+		}
+		i++;
+	}
+	ft_free_matrix((void **)builtin);
+	return (get);	
+}
 /* 
 TODO: função que vê se o coamndo é builtin e volta bool
 int	isbuiltin()
