93b5b10b57e1fae19a2b2e693a7ce3695d00c4ba
adantas- <adantas-@student.42sp.org.br>
2023-05-19 17:38:23 -0300

    M:file on WIP

--- srcs/Tokenizer/token_setup_global.c
+++ srcs/Tokenizer/token_setup_global.c
@@ -1,12 +1,57 @@
 #include "../../includes/minishell.h"
 
-void	set_up_global(char **cmds, size_t nbr_cmds, t_hash *hash)
+static int	fill_son_orders(t_command *son_struct, char *cmd)
 {
-	/* 
-	 * later we think on how to give free to all of this;
-	*/
-	g_shell.envp = hash_to_matrix(hash);
-	g_shell.argv = (char ***)ft_calloc(nbr_cmds + 1, sizeof(char **));
-	g_shell.binp = (char **)ft_calloc(nbr_cmds + 1, sizeof(char *));
-	fill_argv
+	size_t	nbr_args;
+	size_t	index;
+	size_t	arg_index;
+
+	nbr_args = count_args(cmd); // * o loop eh o mesmo
+	if (nbr_args == 0)
+		return (TRUE);
+	son_struct->argv = (char **)ft_calloc(nbr_args + 1, sizeof(char *));
+	copy_args(son_struct->argv, cmd);
+	// ! PUT TO COPY ARGS, NORM ERROR
+	// arg_index = 0;
+	// while (*cmd != '\0')
+	// {
+	// 	if (*cmd == '<' || *cmd == '>')
+	// 		cmd += 2;
+	// 	else if (*cmd == -7)
+	// 		cmd++;
+	// 	else if (*cmd != '<' && *cmd != '>' && *cmd != '\0')
+	// 	{
+	// 		index = 0;
+	// 		while (cmd[index] != '\0' && cmd[index] != -7)
+	// 			index++;
+	// 		son_struct->argv[arg_index] = ft_substr(cmd, 0, index);
+	// 		arg_index++;
+	// 		cmd += index;
+	// 	}
+	// }
+	return (TRUE);
 }
+
+int	set_up_global(char **cmds, size_t nbr_cmds, t_hash *hash)
+{
+	size_t	counter;
+
+	g_shell.command = (t_command *)ft_calloc(nbr_cmds + 1, sizeof(t_command));
+	counter = 0;
+	while (counter < nbr_cmds)
+	{
+		if (fill_son_orders(g_shell.command[counter], *cmds) == FALSE)
+		{
+			free_sons_orders(counter);
+			return (FALSE);
+		}
+		counter++;
+		cmds++;
+	}
+	g_shell.id = 0;
+	g_shell.nbr_sons = nbr_cmds;
+	g_shell.envp = hash_to_matrix(hash);
+}
+/*
+* later we think on how to give free to all of this;
+*/
