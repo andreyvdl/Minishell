e40e3cd879d1184c041b9f169f512310d8b5ee09
Andrey_vdl <81233149+andreyvdl@users.noreply.github.com>
2023-05-23 02:04:37 +0000

    WIP:trying to remove quotes before sending to redirect

---
+++ srcs/Tokenizer/token_remove_quotes.c
@@ -0,0 +1,40 @@
+#include "../../includes/minishell.h"
+
+
+
+static void	copy_edited_str(char *str, char *new)
+{
+	size_t	index;
+
+	new = (char *)ft_calloc(get_edit_size(str) + 1, sizeof(char));
+	index = 0;
+	while (str[index] != '\0')
+	{
+		if (str[index] == '\'' || str[index] == '\"')
+			move_quotes(str, );
+		if (str[index] == '$')
+			expand_var();
+		index++;
+	}
+}
+
+char	**remove_quotes_and_expand(char **matrix)
+{
+	char	**new;
+	char	**free_me;
+	size_t	size;
+
+	if (**matrix == '\0')
+		return (matrix);
+	free_me = matrix;
+	new = (char **)ft_calloc(ft_matrixlen(matrix) + 1, sizeof(char *));
+	size = 0;
+	while (*matrix)
+	{
+		copy_edited_str(*matrix, new[size]);
+		size++;
+		matrix++;
+	}
+	ft_free_matrix(free_me);
+	return (new);
+}
