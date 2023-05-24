d35735ae480b997df90ea6411fcaa37d63940446
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-05-23 23:12:48 -0300

    M:changed son loop because heredoc cancels everything

--- srcs/Tokenizer/token_setup_global.c
+++ srcs/Tokenizer/token_setup_global.c
@@ -3,18 +3,19 @@
 int	set_up_global(char **cmds, size_t nbr_cmds, t_hash *hash)
 {
 	int		status;
-	size_t	counter;
 
 	g_shell.command = (t_command *)ft_calloc(nbr_cmds + 1, sizeof(t_command));
-	counter = 0;
+	g_shell.id = 0;
+	g_shell.nbr_sons = nbr_cmds;
 	g_shell.FREE_THIS_MOTHERFUCKER = cmds;
-	while (counter < nbr_cmds)
+	while (g_shell.id < nbr_cmds)
 	{
-		status = fill_son_orders(g_shell.command[counter], *cmds, hash);
-		counter++;
+		status = fill_son_orders(g_shell.command[g_shell.id], *cmds, hash);
+		if (status == REDI_SIGNAL)
+			return (REDI_SIGNAL);
+		g_shell.id++;
 		cmds++;
 	}
 	g_shell.id = 0;
-	g_shell.nbr_sons = nbr_cmds;
 	g_shell.envp = hash_to_matrix(hash);
 }
