ac0abdf13d76d59d5e1dba09e3ffaa6d0e9dfa41
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-05-23 23:11:21 -0300

    M:changed heredoc, still on WIP

--- srcs/redirections/heredoc.c
+++ srcs/redirections/heredoc.c
@@ -1,28 +1,25 @@
 #include "../../includes/minishell.h"
 
-// void	heredoc()
-// {
-// 	char	*input;
-// 	char	**overview;
-// 	int		j;
+static void	free_son_and_exit(void)
+{
+		ft_free_matrix(g_shell.LEXER_REST);
+		ft_free_matrix(g_shell.REDIRECT_REST);
+		free_hash(g_shell.hash);
+		while (g_shell.id > 0)
+		{
+			if (g_shell.command[g_shell.id].(*argv) != NULL)
+				ft_free_matrix(g_shell.command[g_shell.id].argv);
+			// ! pai abriu, acho q o filho n tem q fechar if (g_shell.command[g_shell.id].wr_here > 0)
+			// !close(g_shell.command[g_shell.id].wr_here);
+			// !if (g_shell.command[g_shell.id].rd_here > 0)
+			// !close(g_shell.command[g_shell.id].rd_here);
+			free(g_shell.command[g_shell.id]);
+			g_shell.id--;
+		}
+		exit(REDI_OK);
+}
 
-// 	while (1)
-// 	{
-// 		input = readline("> ");
-// 		overview = ft_split(expand_vars(input, hash), -7);
-// 		j = 0;
-// 		while (overview && *overview&&overview[j])
-// 		{
-// 			if (ft_strcmp(overview[j], this->delimiter) == 0)
-// 				return;
-// 			j++;
-// 		}
-// 		free(input);
-// 	}
-// 	ft_free_matrix((void **)overview);
-// }
-
-static char	*remove_quotes(char *limiter)
+static void	remove_quotes(char *limiter)
 {
 	while (*limiter)
 	{
@@ -33,22 +30,48 @@ static char	*remove_quotes(char *limiter)
 				limiter++;
 			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1));
 		}
-		else if ()
+		else if (*limiter == '\"')
+		{
+			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1));
+			while (*limiter != '\"' && *limiter)
+				limiter++;
+			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1));
+		}
 	}
 }
 
 static int	without_expansions(char *limiter, t_command *son)
 {
-	char	*temp;
+	int		pid;
+	int		status;
+	char	*line;
 
-	temp = remove_quotes(limiter);
-
+	pid = fork();
+	if (pid == 0)
+	{
+		heredoc_signals();
+		no_expansion_loop(limiter, son);
+		free_son_and_exit();
+	}
+	waitpid(pid, &status, 0);
+	if (status == SIGNAL_INT)
+	{
+		close(son->rd_here);
+		unlink(HEREDOC_FILE);
+		return (REDI_SIGNAL);
+	}
+	return (REDI_OK);
 }
+
+static int	with_expansions
 
 int	heredoc(char *limiter, t_command *son)
 {
+	son->rd_here = open(HEREDOC_FILE, O_RDWR | O_CREAT | O_TRUNC, PERM_CREATE);
 	if (*limiter == '\"' || *limiter == '\'')
+	{
+		remove_quotes(limiter);
 		return (without_expansions(limiter, son));
-	else
-		return (with_expansions(limiter, son));
+	}
+	return (with_expansions(limiter, son));
 }
