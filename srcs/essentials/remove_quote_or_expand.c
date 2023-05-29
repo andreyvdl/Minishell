08a6c67609190b4fa8ec70118f387b1a5423c0fe
adantas- <adantas-@student.42sp.org.br>
2023-05-29 17:51:34 -0300

    +:general expansion file, it will expand or remove quotes or both

---
+++ srcs/essentials/remove_quote_or_expand.c
@@ -0,0 +1,53 @@
+#include "../../includes/minishell.h"
+
+static void	remove_quote(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str == '\'')
+		{
+			ft_memmove(str, str + 1, ft_strlen(str));
+			while (*str != '\'')
+				str++;
+			ft_memmove(str, str + 1, ft_strlen(str));
+		}
+		else if (*str == '\"')
+		{
+			ft_memmove(str, str + 1, ft_strlen(str));
+			while (*str != '\"')
+				str++;
+			ft_memmove(str, str + 1, ft_strlen(str));
+		}
+	}
+}
+
+static char	*solo_expand(char *str)
+{
+	char	*new;
+
+	new = (char *)ft_calloc(solo_size(str) + 1, sizeof(char));
+	solo_copy(new, str);
+	free(str);
+	return (new);
+}
+
+static char	*remove_expand(char *str)
+{
+	char	*new;
+
+	new = (char *)ft_calloc(expand_size(str) + 1, sizeof(char));
+	expand_copy(new, str);
+	free(str);
+	return (new);
+}
+
+char	*remove_quote_or_expand(char *str)
+{
+	if ((ft_strchr(str, '\'') || ft_strchr(str, '\"')) && ft_strchr(str, '$'))
+		return (remove_expand(str));
+	else if (ft_strchr(str, '$'))
+		return (solo_expand(str));
+	else if (ft_strchr(str, '\'') || ft_strchr(str, '\"'))
+		remove_quote(str);
+	return (str);
+}
