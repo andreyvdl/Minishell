e31b56da80e8b501e4dc6fb2bc34e31ba4c10e4a
adantas- <adantas-@student.42sp.org.br>
2023-06-01 17:52:50 -0300

    M:fixed infinite loop and changed names

--- srcs/redirections/heredoc.c
+++ srcs/redirections/heredoc.c
@@ -2,21 +2,21 @@
 
 static void	free_son_and_exit(void)
 {
-		ft_free_matrix((void **)g_shell.LEXER_REST);
-		ft_free_matrix((void **)g_shell.REDIRECT_REST);
-		free_hash(g_shell.hash);
-		while (g_shell.id > 0)
-		{
-			if (*g_shell.command[g_shell.id].argv != NULL)
-				ft_free_matrix((void **)g_shell.command[g_shell.id].argv);
-			// ! pai abriu, acho q o filho n tem q fechar if (g_shell.command[g_shell.id].wr_here > 0)
-			// !close(g_shell.command[g_shell.id].wr_here);
-			// !if (g_shell.command[g_shell.id].rd_here > 0)
-			// !close(g_shell.command[g_shell.id].rd_here);
-			g_shell.id--;
-		}
-		free(g_shell.command);
-		exit(REDI_OK);
+	ft_free_matrix((void **)g_shell.pipeline_rest);
+	ft_free_matrix((void **)g_shell.redirect_rest);
+	free_hash(g_shell.hash);
+	while (g_shell.id > 0)
+	{
+		if (*g_shell.command[g_shell.id].argv != NULL)
+			ft_free_matrix((void **)g_shell.command[g_shell.id].argv);
+		// ! pai abriu, acho q o filho n tem q fechar if (g_shell.command[g_shell.id].wr_here > 0)
+		// !close(g_shell.command[g_shell.id].wr_here);
+		// !if (g_shell.command[g_shell.id].rd_here > 0)
+		// !close(g_shell.command[g_shell.id].rd_here);
+		g_shell.id--;
+	}
+	free(g_shell.command);
+	exit(REDI_OK);
 }
 
 static void	remove_quotes_heredoc(char *limiter)
@@ -25,17 +25,17 @@ static void	remove_quotes_heredoc(char *limiter)
 	{
 		if (*limiter == '\'')
 		{
-			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1));
+			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1) + 1);
 			while (*limiter != '\'')
 				limiter++;
-			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1));
+			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1) + 1);
 		}
 		else if (*limiter == '\"')
 		{
-			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1));
+			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1) + 1);
 			while (*limiter != '\"')
 				limiter++;
-			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1));
+			ft_memmove(limiter, limiter + 1, ft_strlen(limiter + 1) + 1);
 		}
 		else
 			limiter++;
