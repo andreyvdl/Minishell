8d783e184504629a41583af1358ecf2edd754a21
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-06-02 20:00:29 -0300

    M:changed default writer and close condition

--- srcs/redirections/trunc.c
+++ srcs/redirections/trunc.c
@@ -11,13 +11,13 @@ int	redirect_output_trunc(char *filename, t_command *son, size_t id)
 		free(temp);
 	if (file_des == -1)
 	{
-		if (son[id].wr_here > -1)
+		if (son[id].wr_here > STDOUT_FILENO)
 			close(son[id].wr_here);
 		son[id].wr_here = -42;
 		perror(ERR_WRITING);
 		return (REDI_ERR);
 	}
-	if (son[id].wr_here > -1)
+	if (son[id].wr_here > STDOUT_FILENO)
 		close(son[id].wr_here);
 	son[id].wr_here = file_des;
 	return (REDI_OK);
