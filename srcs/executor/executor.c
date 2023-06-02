c098717eb9841c53385bf4642a8ad8d30e23d1aa
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-06-02 19:57:21 -0300

    M:redo of the executor for async execution

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -1,6 +1,6 @@
 #include "../../includes/minishell.h"
 
-static char **bins_path()
+/* static char **bins_path()
 {
 	return (ft_split(search(g_shell.hash, "PATH"), ':'));
 }
@@ -77,15 +77,57 @@ static	void	runtime(char **bins)
 		}
 		bins++;
 	}
+} */
+
+static void	status_after_execution(int status)
+{
+	char	*nbr;
+
+	nbr = ft_itoa(status);
+	insert_node(g_shell.hash, "?", nbr);
+	free(nbr);
+}
+
+static void	wait_for_them(pid_t pid, int *pipe)
+{
+	int	status;
+
+	close(pipe[0]);
+	close(pipe[1]);
+	waitpid(pid, &status, NONE);
+	while (g_shell.id > 1)
+	{
+		wait(NULL);
+		g_shell.id--;
+	}
+	if (WIFEXITED(status))
+		status_after_execution(WEXITSTATUS(status));
+	else if (WIFSIGNALED(status))
+		status_after_execution(WTERMSIG(status) + 128);
 }
 
 void	executor()
 {
-	if (isbuiltin(g_shell.command->argv[0]))
-		builtins();
-	else
+	int		pipe_fd[2];
+	pid_t	pid;
+
+	if (pipe(pipe_fd) == -1)
 	{
-		runtime(path_creator(g_shell.command->argv[0]));
-		ft_free_matrix((void **)g_shell.command->argv);
+		perror(ERR_PIPE);
+		return ;
 	}
+	while (g_shell.id < g_shell.nbr_sons)
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			perror(ERR_FORK);
+			kill(0, SIGKILL);
+			return ;
+		}
+		if (pid == 0)
+			guide_sons(pipe_fd, g_shell.id);
+		g_shell.id++;
+	}
+	wait_for_them(pid, pipe_fd);
 }
