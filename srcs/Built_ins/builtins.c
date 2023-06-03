077a773305851b8480960d7163c78bd1e265efaa
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-06-03 17:48:13 -0300

    M:just comment to compile, need to be made WIP

--- srcs/Built_ins/builtins.c
+++ srcs/Built_ins/builtins.c
@@ -1,19 +1,19 @@
 #include "../../includes/minishell.h"
 
-int	builtins(char **argv)
+void	builtins(void)
 {
-	if (ft_strcmp(*argv, "cd") == 0)
-		return (cd());
-	else if (ft_strcmp(*argv, "echo") == 0)
-		return (echo());
-	else if (ft_strcmp(*argv, "env") == 0)
-		return (env());
-	else if (ft_strcmp(*argv, "pwd") == 0)
-		return (pwd());
-	else if (ft_strcmp(*argv, "exit") == 0)
-		return (mini_exit());
-	else if (ft_strcmp(*argv, "export") == 0)
-		return (export());
-	else if (ft_strcmp(*argv, "unset") == 0)
-		return (unset());
+	// if (ft_strcmp(*argv, "cd") == 0)
+	// 	return (cd());
+	// else if (ft_strcmp(*argv, "echo") == 0)
+	// 	return (echo());
+	// else if (ft_strcmp(*argv, "env") == 0)
+	// 	return (env());
+	// else if (ft_strcmp(*argv, "pwd") == 0)
+	// 	return (pwd());
+	// else if (ft_strcmp(*argv, "exit") == 0)
+	// 	return (mini_exit());
+	// else if (ft_strcmp(*argv, "export") == 0)
+	// 	return (export());
+	// else if (ft_strcmp(*argv, "unset") == 0)
+	// 	return (unset());
 }
