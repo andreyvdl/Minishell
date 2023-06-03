2ed8bcc22b8c43632d5ba967baf807f90a085b32
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-06-03 17:50:33 -0300

    M:guide the sons to their fds and if is a builtin or system call

--- srcs/executor/guide_sons.c
+++ srcs/executor/guide_sons.c
@@ -4,41 +4,29 @@ static void	execute_cmd(size_t id)
 {
 	if (g_shell.command[id].argv != NULL && \
 	isbuiltin(*g_shell.command[id].argv) == TRUE)
-		exit(builtins(g_shell.command[id].argv));
+	{
+		ft_printf_ln("builtins on WIP, check github for more info");
+		free_son();
+		exit(EXIT_SUCCESS);
+	}
 	else if (g_shell.command[id].argv != NULL && \
 	*g_shell.command[id].argv != NULL)
-		system_exec(g_shell.command[id].argv, g_shell.envp, id);
-	else
-	{
-		free_son();
-		if (g_shell.command[id].argv != NULL)
-			ft_printf_fd(STDERR_FILENO, ERR_CMD_NOT_FOUND, \
-			*g_shell.command[id].argv);
-		else
-			ft_printf_fd(STDERR_FILENO, ERR_CMD_NOT_FOUND, NULL);
-		exit(CMD_NOT_FOUND);
-	}
+		system_exec(g_shell.command[id].argv, g_shell.envp);
 }
 
 void	guide_sons(int *pipe, size_t id)
 {
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
 	if (g_shell.command[id].rd_here < STDIN_FILENO || \
 	g_shell.command[id].wr_here < STDOUT_FILENO)
 	{
 		close(pipe[0]);
 		close(pipe[1]);
 		free_son();
-		exit(FILE_ERROR);
+		exit(EXIT_FAILURE);
 	}
-	if (dup2(pipe[0], g_shell.command[id].rd_here) == -1 || \
-	dup2(pipe[1], g_shell.command[id].wr_here) == -1) // 2 instruções em uma linha??????????????
-	{
-		perror(ERR_DUP);
-		close(pipe[0]);
-		close(pipe[1]);
-		free_son();
-		exit(FILE_ERROR);
-	}
+	open_redirect(pipe, id);
 	close(pipe[0]);
 	close(pipe[1]);
 	execute_cmd(id);
