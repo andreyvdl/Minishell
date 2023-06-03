c7bdb5593ed2146bb68e3f650d7b311f32eab59d
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-06-03 17:45:47 -0300

    +:source file that makes the final check before execution

---
+++ srcs/executor/execution_check.c
@@ -0,0 +1,27 @@
+#include "../../includes/minishell.h"
+
+void	execution_check(char *path)
+{
+	if (filename_too_big(path) == TRUE)
+	{
+		ft_printf_fd(STDERR_FILENO, ERR_FILENAME, strerror(FT_ENAMETOOLONG));
+		free(path);
+		free_son();
+		exit(EXEC_WENT_WRONG);
+	}
+	is_directory(path);
+	if (access(path, F_OK) == -1)
+	{
+		ft_printf_fd(STDERR_FILENO, ERR_CMD_NOT_FOUND, path);
+		free(path);
+		free_son();
+		exit(CMD_NOT_FOUND);
+	}
+	else if (access(path, X_OK) == -1)
+	{
+		free(path);
+		free_son();
+		perror(ERR_ACCESS);
+		exit(EXEC_WENT_WRONG);
+	}
+}
