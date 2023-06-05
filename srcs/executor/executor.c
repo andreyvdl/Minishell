6ff80857fac605c6bbf80961c9f26486428f7179
adantas- <adantas-@student.42sp.org.br>
2023-06-05 14:56:35 -0300

    M:saved original fd and put the rest of the code in the norm

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -88,12 +88,10 @@ static void	status_after_execution(int status)
 	free(nbr);
 }
 
-static void	wait_for_them(pid_t pid, int *pipe)
+static void	wait_for_them(pid_t pid)
 {
 	int	status;
 
-	close(pipe[0]);
-	close(pipe[1]);
 	waitpid(pid, &status, NONE);
 	while (g_shell.id > 1)
 	{
@@ -106,28 +104,47 @@ static void	wait_for_them(pid_t pid, int *pipe)
 		status_after_execution(WTERMSIG(status) + 128);
 }
 
+static int	forking(pid_t *pid, int *pipe, int fd_original)
+{
+	*pid = fork();
+	if (*pid == -1)
+	{
+		perror(ERR_FORK);
+		kill(0, SIGKILL);
+		return (EXIT_FAILURE);
+	}
+	if (*pid == 0)
+	{
+		close(fd_original);
+		guide_sons(pipe, g_shell.id);
+	}
+	return (EXIT_SUCCESS);
+}
+
 void	executor()
 {
 	int		pipe_fd[2];
+	int		backup_fd;
 	pid_t	pid;
 
-	if (pipe(pipe_fd) == -1)
-	{
-		perror(ERR_PIPE);
-		return ;
-	}
+	backup_fd = dup(STDIN_FILENO);
 	while (g_shell.id < g_shell.nbr_sons)
 	{
-		pid = fork();
-		if (pid == -1)
+		if (pipe(pipe_fd) == -1)
 		{
-			perror(ERR_FORK);
-			kill(0, SIGKILL);
+			perror(ERR_PIPE);
+			status_after_execution(EXEC_WENT_WRONG);
 			return ;
 		}
-		if (pid == 0)
-			guide_sons(pipe_fd, g_shell.id);
+		if (forking(&pid, pipe_fd, backup_fd) == EXIT_FAILURE)
+			break ;
+		close(STDIN_FILENO);
+		dup2(pipe_fd[0], STDIN_FILENO);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
 		g_shell.id++;
 	}
-	wait_for_them(pid, pipe_fd);
+	dup2(backup_fd, STDIN_FILENO);
+	close(backup_fd);
+	wait_for_them(pid);
 }
