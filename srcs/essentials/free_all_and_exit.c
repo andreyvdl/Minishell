446241c0e237bdb4d167331baa0065eb104254d3
adantas- <adantas-@student.42sp.org.br>
2023-06-05 14:54:21 -0300

    M:closing fds

--- srcs/essentials/free_all_and_exit.c
+++ srcs/essentials/free_all_and_exit.c
@@ -5,5 +5,8 @@ void	free_all_and_exit(t_hash *hash)
 	ft_putstr("exit\n");
 	free_hash(hash);
 	rl_clear_history();
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
 	exit(0);
 }
