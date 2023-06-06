64f50d2ca46640f37e9314a1e9b68fdcb9a22f08
Andrey Dantas Leal <adantas-@c3r10p4.42sp.org.br>
2023-06-06 17:45:25 -0300

    M:son cd only detects errors

--- srcs/Built_ins/cd.c
+++ srcs/Built_ins/cd.c
@@ -1,6 +1,6 @@
 #include "../../includes/minishell.h"
 
-int change_directory(const char *path)
+/* int change_directory(const char *path)
 {
 	char current_dir[256];
 
@@ -40,4 +40,64 @@ int cd()
 		return (ft_printf("to many args\n"));
 
 	return (0);
+} */
+
+static void	fake_go_to_home(void)
+{
+	char	*home;
+
+	home = search(g_shell.hash, "HOME");
+	if (home == NULL)
+	{
+		ft_printf_fd(STDERR_FILENO, ERR_CD_HOME);
+		free_son();
+		exit(EXIT_FAILURE);
+	}
+	free_son();
+	exit(EXIT_SUCCESS);
+}
+
+static void	has_many_args(char **argv)
+{
+	if (*(argv + 1) != NULL && *(argv + 2) != NULL)
+	{
+		ft_printf_fd(STDERR_FILENO, ERR_CD_ARGS);
+		free_son();
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void	cd_is_directory(char **argv)
+{
+	struct stat	file_stat;
+
+	file_stat = (struct stat){0};
+	if (stat(*(argv + 1), &file_stat) == -1)
+	{
+		free_son();
+		perror(ERR_STAT);
+		exit(EXIT_FAILURE);
+	}
+	if (S_ISDIR(file_stat.st_mode))
+		return ;
+	ft_printf_fd(STDERR_FILENO, ERR_CD_DIR, *(argv + 1));
+	free_son();
+	exit(EXIT_FAILURE);
+}
+
+void	ft_cd(char **argv)
+{
+	has_many_args(argv);
+	if (*(argv + 1) == NULL || **(argv + 1) == '\0' || \
+	(**(argv + 1) == '~' && *(*(argv + 1) + 1) == '\0'))
+		fake_go_to_home();
+	if (access(*(argv + 1), X_OK) == -1)
+	{
+		perror(ERR_CD_ACCESS);
+		free_son();
+		exit(EXIT_FAILURE);
+	}
+	cd_is_directory(argv);
+	free_son();
+	exit(EXIT_SUCCESS);
 }
