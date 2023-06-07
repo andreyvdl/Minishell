7f36f6f4a99f49922db5639fd887275ee50c2bd5
adantas- <adantas-@student.42sp.org.br>
2023-06-07 18:03:24 -0300

    M:only left exit and unset

--- srcs/Built_ins/builtins.c
+++ srcs/Built_ins/builtins.c
@@ -2,18 +2,18 @@
 
 void	builtins(char **argv)
 {
-		if (ft_strcmp(*argv, "echo") == 0)
-			ft_echo(argv);
-		else if (ft_strcmp(*argv, "cd") == 0)
-			ft_cd(argv);
-		else if (ft_strcmp(*argv, "export") == 0)
-			ft_export(argv);
-		// else if (ft_strcmp(*argv, "env") == 0)
-		// 	return (env());
-		// else if (ft_strcmp(*argv, "pwd") == 0)
-		// 	return (pwd());
-		// else if (ft_strcmp(*argv, "exit") == 0)
-		// 	return (mini_exit());
-		// else if (ft_strcmp(*argv, "unset") == 0)
-		// 	return (unset());
+	if (ft_strcmp(*argv, "echo") == 0)
+		ft_echo(argv);
+	else if (ft_strcmp(*argv, "cd") == 0)
+		ft_cd(argv);
+	else if (ft_strcmp(*argv, "export") == 0)
+		ft_export(argv);
+	else if (ft_strcmp(*argv, "env") == 0)
+		ft_env(argv);
+	else if (ft_strcmp(*argv, "pwd") == 0)
+		ft_pwd();
+	// else if (ft_strcmp(*argv, "exit") == 0)
+	// 	return (mini_exit());
+	// else if (ft_strcmp(*argv, "unset") == 0)
+	// 	return (unset());
 }
