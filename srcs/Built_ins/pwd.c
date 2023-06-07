33c91317470d43fdbc626d709db064079736ae08
adantas- <adantas-@student.42sp.org.br>
2023-06-07 18:04:43 -0300

    M:need to test pwd

--- srcs/Built_ins/pwd.c
+++ srcs/Built_ins/pwd.c
@@ -1,6 +1,6 @@
 #include "../../includes/minishell.h"
 
-void	pwd()
+/* void	pwd()
 {
 	char	path[4096];
 
@@ -8,4 +8,30 @@ void	pwd()
 		printf("%s\n", path);
 	else
 		perror("pwd");
+} */
+
+void	ft_pwd(void)
+{
+	char	*cwd;
+
+	cwd = NULL;
+	cwd = getcwd(NULL, 0);
+	if (cwd == NULL)
+	{
+		perror(ERR_PWD_PERROR);
+		free_son();
+		exit(EXIT_FAILURE);
+	}
+	if (ft_strlen(cwd) > FT_PATHMAX)
+	{
+		ft_printf_fd(STDERR_FILENO, ERR_PWD_TOO_LONG, cwd, \
+		strerror(FT_ENAMETOOLONG));
+		free(cwd);
+		free_son();
+		exit(EXIT_FAILURE);
+	}
+	ft_putstr(cwd);
+	free(cwd);
+	free_son();
+	exit(EXIT_SUCCESS);
 }
