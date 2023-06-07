983d25e93831ff86f5869f764ff498cc37960357
adantas- <adantas-@student.42sp.org.br>
2023-06-07 18:04:04 -0300

    M:need to test env

--- srcs/Built_ins/env.c
+++ srcs/Built_ins/env.c
@@ -1,6 +1,6 @@
 #include "../../includes/minishell.h"
 
-void	env()
+/* void	env()
 {
 	char	**env;
 	int		i;
@@ -11,4 +11,58 @@ void	env()
 		return ((void)ft_printf("Command is: env!"));
 	while (env && env[i])
 		ft_printf("%s\n", env[i++]);
+} */
+
+static void	env_is_directory(char **argv)
+{
+	struct stat	file_stat;
+
+	file_stat = (struct stat){0};
+	if (stat(*(argv + 1), &file_stat) == -1)
+	{
+		free_son();
+		perror(ERR_STAT);
+		exit(EXIT_FAILURE);
+	}
+	if (S_ISDIR(file_stat.st_mode))
+		return (TRUE);
+	return (FALSE);
+}
+
+static void	print_vars_env(void)
+{
+	t_node	*node;
+	size_t	looper;
+
+	looper = 0;
+	while (looper < HASH_SIZE)
+	{
+		node = g_shell.hash->list[looper];
+		while (node != NULL)
+		{
+			if (node->value != NULL)
+				ft_printf_ln("%s=%s", node->key, node->value);
+			node = node->next;
+		}
+		looper++;
+	}
+	free_son();
+	exit(EXIT_SUCCESS);
+}
+
+void	ft_env(char **argv)
+{
+	if (*(argv + 1) != NULL && env_is_directory(argv) == TRUE)
+	{
+		ft_printf_fd(STDERR_FILENO, ERR_ENV_ARGS);
+		free_son();
+		exit(EXEC_WENT_WRONG);
+	}
+	else if (*(argv + 1) != NULL)
+	{
+		ft_printf_fd(STDERR_FILENO, ERR_ENV_ARGS);
+		free_son();
+		exit(CMD_NOT_FOUND);
+	}
+	print_vars_env();
 }
