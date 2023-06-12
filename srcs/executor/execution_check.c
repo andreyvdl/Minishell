04dd7fa2ccfce06c2892a5fb16e51c0a813de9df
adantas- <adantas-@student.42sp.org.br>
2023-06-12 17:43:19 -0300

    M:closed son fds in case of error

--- srcs/executor/execution_check.c
+++ srcs/executor/execution_check.c
@@ -7,6 +7,7 @@ void	execution_check(char *path)
 		ft_printf_fd(STDERR_FILENO, ERR_FILENAME, strerror(FT_ENAMETOOLONG));
 		free(path);
 		free_son();
+		close_std_error();
 		exit(EXEC_WENT_WRONG);
 	}
 	is_directory(path);
@@ -15,6 +16,7 @@ void	execution_check(char *path)
 		ft_printf_fd(STDERR_FILENO, ERR_CMD_NOT_FOUND, path);
 		free(path);
 		free_son();
+		close_std_error();
 		exit(CMD_NOT_FOUND);
 	}
 	else if (access(path, X_OK) == -1)
@@ -22,6 +24,7 @@ void	execution_check(char *path)
 		free(path);
 		free_son();
 		perror(ERR_ACCESS);
+		close_std_error();
 		exit(EXEC_WENT_WRONG);
 	}
 }
