8769f0c0535aa6ef788d77920b04f097cc8eaf92
adantas- <adantas-@student.42sp.org.br>
2023-06-12 17:45:09 -0300

    M:fix status code and close fds in case of error

--- srcs/executor/is_directory.c
+++ srcs/executor/is_directory.c
@@ -9,13 +9,15 @@ void	is_directory(char *path)
 	{
 		free_son();
 		perror(ERR_STAT);
-		exit(EXEC_WENT_WRONG);
+		close_std_error();
+		exit(CMD_NOT_FOUND);
 	}
 	if (S_ISDIR(file_stat.st_mode))
 	{
 		free_son();
 		free(path);
 		ft_printf_fd(STDERR_FILENO, ERR_EXEC_DIR, strerror(FT_EISDIR));
+		close_std_error();
 		exit(EXEC_WENT_WRONG);
 	}
 }
