de7b1c91746ac49fa7e16e8bd1f542691fbfd8ba
adantas- <adantas-@student.42sp.org.br>
2023-06-12 17:44:19 -0300

    M:son now clear the history

--- srcs/executor/free_son.c
+++ srcs/executor/free_son.c
@@ -1,30 +1,13 @@
 #include "../../includes/minishell.h"
 
-static void	free_son_hash(t_hash *hash)
-{
-	size_t	looper;
-	t_node	*node;
-
-	looper = 0;
-	while (looper < HASH_SIZE)
-	{
-		node = hash->list[looper];
-		if (node != NULL)
-		{
-			free(node->key);
-			free(node->value);
-			free(node);
-		}
-		looper++;
-	}
-}
-
 void	free_son(void)
 {
 	size_t	looper;
 
 	looper = 0;
-	while (looper < g_shell.nbr_sons)
+	ft_free_matrix((void **)g_shell.envp);
+	free_hash(g_shell.hash);
+	while (looper <= g_shell.nbr_sons)
 	{
 		ft_free_matrix((void **)g_shell.command[looper].argv);
 		if (g_shell.command[looper].wr_here > STDOUT_FILENO)
@@ -33,7 +16,13 @@ void	free_son(void)
 			close(g_shell.command[looper].rd_here);
 		looper++;
 	}
-	ft_free_matrix((void **)g_shell.envp);
 	free(g_shell.command);
-	free_son_hash(g_shell.hash);
+	rl_clear_history();
+}
+
+void	close_std_error(void)
+{
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
 }
